19a7b4be580496a65ad16adbd5438d809b558085
liuxiaoyu-git <xiaoyliu@redhat.com>
2020-01-18 11:04:43 +0800

    Update run_mTLS.sh

--- scripts/run_mTLS.sh
+++ scripts/run_mTLS.sh
@@ -1,13 +1,14 @@
-                                                                                                        
-export CURL_POD=$(oc4 get pods -l app=curl | grep curl | awk '{ print $1}' )              
-export CUSTOMER_POD=$(oc4 get pods -l app=customer | grep customer | awk '{ print $1}' )  
+                                                                                                     
+export CURL_POD=$(oc get pods -l app=curl | grep curl | awk '{ print $1}' )              
+export CUSTOMER_POD=$(oc get pods -l app=customer | grep customer | awk '{ print $1}' )  
                                                                                                             
 while :; do                                                                                                 
                                                                                                             
 echo "Executing curl in curl pod"                                                                           
 oc exec $CURL_POD curl http://preference:8080                                            
-                                                                                                            
+echo "--------------------------------------------------------------------------------"
 echo "Executing curl in customer pod"                                                                       
 oc exec $CUSTOMER_POD -c customer curl http://preference:8080                            
+echo "--------------------------------------------------------------------------------"
                                                                                                             
 done                                                                                                        
